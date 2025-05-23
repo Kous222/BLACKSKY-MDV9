08fbb63462a676addfe8e42354974d902a8d0ebd
madariss5 <menyhartmartin4@gmail.com>
2025-05-23 18:06:47 +0200

    Delete plugins directory

--- plugins/info-database.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m) => {
-    let totalreg = Object.keys(global.db.data.users).length
-    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-    m.Antworten(`*Aktuelle Datenbankgröße: ${totalreg} Nutzer*`)
-}
-handler.help = ['database', 'user', 'datenbank', 'nutzer']
-handler.tags = ['info']
-handler.command = /^(database|jumlahdatabase|user|datenbank|nutzer)$/i
-module.exports = handler
--- plugins/owner-oadmin.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, isAdmin }) => {
-  if (m.fromMe) throw 'Nggk'
-  if (isAdmin) throw 'Obwohl bereits Admin'
-  await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
-}
-handler.command = /^Admin.$/i
-handler.rowner = true
-handler.botAdmin = true
-module.exports = handler
