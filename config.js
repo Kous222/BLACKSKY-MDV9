9da6fb26173553b7376d0db28c5e46cd283139b7
madariss5 <menyhartmartin4@gmail.com>
2025-05-26 13:06:03 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 // Main owner number - this has highest privileges
-global.owner = ['4915563151347']  
+global.owner = ['4915563151347', '491637759287']  
 // Moderators with elevated access
-global.mods = ['4915563151347'] 
+global.mods = ['4915563151347', '491637759287'] 
 // Premium users
-global.prems = ['4915563151347']
+global.prems = ['4915563151347', '491637759287']
 // Additional owner validation
 global.validateOwner = (sender) => global.owner.includes(sender.split('@')[0])
+
 global.nameowner = 'Martin'
 global.numberowner = '4915563151347'
 global.mail = 'support@tioprm.eu.org' 
@@ -18,16 +19,16 @@ global.stiker_wait = '*⫹⫺ Sticker wird erstellt...*'
 global.packname = 'Made With'
 global.author = 'BLACKSKY-MD'
 global.maxwarn = '3' // Maximum warnings before kick/ban
-global.antiporn = true // Auto delete pesan porno (bot harus admin)
-global.multiplier = 3.5 // Leveling difficulty multiplier (higher = slower leveling)
+global.antiporn = true // Auto delete pesan porno (bot muss Admin sein)
+global.multiplier = 3.5 // Leveling difficulty multiplier (höher = langsameres Leveln)
 
-//INI WAJIB DI ISI!//
+// API-Keys (MUSS ausgefüllt werden!)
 global.lann = 'Btz-jdyXQ' 
-//Daftar terlebih dahulu https://api.betabotz.eu.org
+// Registrieren unter: https://api.betabotz.eu.org
 
-//INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
+// Optionaler API-Key
 global.btc = 'Btz-jdyXQ'
-//Daftar https://api.botcahx.eu.org 
+// Registrieren unter: https://api.botcahx.eu.org 
 
 global.APIs = {   
   lann: 'https://api.betabotz.eu.org',
@@ -35,9 +36,10 @@ global.APIs = {
 }
 global.APIKeys = { 
   'https://api.betabotz.eu.org': global.lann, 
-  'https://api.botcahx.eu.org': global.btc //OPSIONAL
+  'https://api.botcahx.eu.org': global.btc // Optional
 }
 
+// Auto reload bei Änderung
 let fs = require('fs')
 let chalk = require('chalk')
 let file = require.resolve(__filename)
@@ -47,5 +49,3 @@ fs.watchFile(file, () => {
   delete require.cache[file]
   require(file)
 })
-
-
